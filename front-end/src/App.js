4aa2f904e7aa4a3a11cea0ede2ff6c6986ea8c07
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2020-08-23 16:48:18 +0700

    fetch jokes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    fetchJokes();
+  }, []);
+
+  const fetchJokes = async () => {
+    const res = await axios.get(`${ROOT_URL}/3`);
+    setJokes(res.data.value);
+  };
+
+  const ROOT_URL = 'https://api.icndb.com/jokes/random';
+
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <ul>
+        {jokes.map(({ joke }) => (
+          <li>{joke}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
